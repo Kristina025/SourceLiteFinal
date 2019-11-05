931af771785824425973fd27f58b33f82981fb6b
Kristina <kristina@Kristinas-MacBook-Pro.local>
2019-11-05 18:00:01 -0500

    Added button, changed title, and changed subtitle

--- FinalProjectJavaScript.js
+++ FinalProjectJavaScript.js
@@ -3,7 +3,10 @@ var request = new XMLHttpRequest();
 function firstAPI() {
     request.open('GET', 'https://dog.ceo/api/breeds/image/random')
     request.onload = function() {
-        console.log(request.response)
+        //need to create image element 
+        object = JSON.parse(request.response)
+        image = object.message 
+        console.log(image)
     }
     request.send()
 }
